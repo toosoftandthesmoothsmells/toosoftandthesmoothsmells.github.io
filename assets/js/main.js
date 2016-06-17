4cc742cb417760e275b9a9d99a8e3d1da7d88e07
too <too@uelva.com>
2016-06-17 11:57:49 -0500

    tpi

--- assets/js/main.js
+++ assets/js/main.js
@@ -39,9 +39,9 @@
 
 					// Images (in the format of 'url': 'alignment').
 						images: {
-							'images/bg04.jpg': 'center',
-							'images/bg05.jpg': 'center',
-							'images/bg06.jpg': 'center'
+							'images/bg01.jpg': 'center',
+							'images/bg02.jpg': 'center',
+							'images/bg03.jpg': 'center'
 						},
 
 					// Delay.
